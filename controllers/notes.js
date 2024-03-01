97bc731f2edc042e8b4a8410ddd7c0ce9be92543
Himanshu kumar <himanshugupta251280@gmail.com>
2024-03-01 19:39:12 +0530

    test console

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,7 +5,7 @@ import Note from '../models/note.js';
 // Get all notes
 export const getNotes = async (req, res) => {
   try {
-    console.log(new Date.now())
+    console.log("Hiiii")
     // const notes = await Note.find();
     // res.status(200).json(notes);
     res.status(200).json({"Hello":"Hii"})
